066b3ed0c33f61a2a8efc5c3e511b52d47a33657
ZzzhaoGZ <1569389459@qq.com>
2023-08-27 14:00:04 +0800

    merge interval

--- 20230827/merge_interval.py
+++ 20230827/merge_interval.py
@@ -4,3 +4,15 @@
 # @File : merge_interval.py
 # @Software: PyCharm
 
+# Definition for an interval.
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans = [intervals[0]]
+        for s, e in intervals[1:]:
+            if ans[-1][1] < s:
+                ans.append([s, e])
+            else:
+                ans[-1][1] = max(ans[-1][1], e)
+        return ans
